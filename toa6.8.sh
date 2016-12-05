e19956cded0e9510462c48b2e80890225c13d2c4
xianhua <xianhua@meizu.com>
2016-12-05 14:28:57 +0800

    'toa'

--- toa6.8.sh
+++ toa6.8.sh
@@ -12,5 +12,9 @@ patch -p1 < ~/rpmbuild/SOURCES/centos6.8-toa.patch
 cd ~/rpmbuild/SOURCES
 tar -cjvf linux-2.6.32-642.11.1.el6.tar.bz2 linux-2.6.32-642.11.1.el6
 
+sed -i 's/# % define buildid .local/%define buildid .toa/g' ~/rpmbuild/SPECS/kernel.spec
 rpmbuild -bb ~/rpmbuild/SPECS/kernel.spec
+echo 'build the toa kernel rpm list \n'
 ls -alt ~/rpmbuild/RPMS/x86_64/
+echo 'every realserver is to install ~/rpmbuild/RPMS/x86_64/kernel-2.6.32-642.11.1.el6.toa.x86_64.rpm \n'
+echo 'after install ,reboot realserver \n'
--- toa7.2.sh
+++ toa7.2.sh
@@ -15,5 +15,9 @@ cd ~/rpmbuild/SOURCES
 tar cvf linux-3.10.0-327.36.3.el7.tar linux-3.10.0-327.36.3.el7
 xz -z linux-3.10.0-327.36.3.el7.tar
 
+sed -i 's/# % define buildid .local/%define buildid .toa/g' ~/rpmbuild/SPECS/kernel.spec
 rpmbuild -bb ~/rpmbuild/SPECS/kernel.spec
+echo 'build the toa kernel rpm list \n'
 ls -alt ~/rpmbuild/RPMS/x86_64/
+echo 'every realserver is to install ~/rpmbuild/RPMS/x86_64/kernel-3.10.0-327.36.3.el7.toa.x86_64.rpm \n'
+echo 'after install ,reboot realserver \n'
